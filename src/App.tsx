1bff6554c412b19a7aed73fb71d680650b3bcd03
Amumug <fabriali4@gmail.com>
2024-08-11 18:17:06 +0100

    react-form

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex justify-center items-end gap-11'>
+      <h1 className=' text-[#000]'>Hello World</h1>
+      <div className=''>Heloo World</div>
+
+
+      </div>
+
+      </>
+  )
+}
+
+export default App
