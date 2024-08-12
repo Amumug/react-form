70d4346566d5820cbb73f3f226eabbcceb1f3ddc
Amumug <fabriali4@gmail.com>
2024-08-12 13:20:06 +0100

    react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
-  return (
-    <>
-      <div className='flex justify-center items-end gap-11'>
-      <h1 className=' text-[#000]'>Hello World</h1>
-      <div className=''>Heloo World</div>
-    sdas
-
-      </div>
-
-      </>
-  )
+  return <>
+  </>;
 }
 
-export default App
+export default App;
